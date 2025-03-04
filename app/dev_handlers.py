e07cd47845c90f17271da42410987c2992752d62
riddler_rid <unkownrid@gmail.com>
2025-03-04 13:02:59 +0300

    add sqlite database

--- app/dev_handlers.py
+++ app/dev_handlers.py
@@ -1,6 +1,10 @@
 from aiogram import Router
 from aiogram.filters import Command
 from aiogram.types import Message
+from sqlalchemy import select
+
+from app.database import create_tables, async_session
+from app.models import User
 
 dev_router = Router()
 
@@ -8,3 +12,17 @@ dev_router = Router()
 @dev_router.message(Command('dev'))
 async def get_dev(message: Message) -> None:
     await message.answer("Dev!!!")
+
+
+@dev_router.message(Command('create'))
+async def create_database(message: Message) -> None:
+    await create_tables()
+    await message.answer("Create tables!!!")
+
+
+@dev_router.message(Command('users'))
+async def get_users(message: Message) -> None:
+    async with async_session() as session:
+        result = await session.execute(select(User).order_by(User.id))
+        users = result.scalars().all()
+        await message.answer(f"users: {users}")
